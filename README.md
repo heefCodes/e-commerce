0d5c3d83d1815570e219c53aaeb689f7dcf4097a
heefCodes <abdulkareemtoyin277@gmail.com>
2023-06-13 11:16:50 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# E-Commerce application
+# step 1: Create react app
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+function App() {
+    return (
+        <div className='grid-container'>
+            <header>
+                <a href="/">E-commerce</a>
+            </header>
+            <main>
+                Product list
+            </main>
+            <footer>
+                All right is reserved.
+            </footer>
+
+        </div>
+    );
+}
+
+export default App;
